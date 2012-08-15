800b09f255a6d440dc2e6315c17497f15e0c4774
Karl Anderson <kra@monkey.org>
2012-08-14 19:30:43 -0700

    FIRST POST

---
+++ test.py
@@ -0,0 +1,17 @@
+import serial
+import time
+import random
+
+dev = "/dev/tty.usbmodem12341"
+start_event = 'START'
+end_event = 'END'
+beat_event = 'BEAT'
+addr = '1'
+
+ser = serial.Serial(dev)
+
+ser.write('%s%s:%s\n' % (start_event, addr, 2))
+while True:
+    ser.write('%s%s:%s\n' % (beat_event, addr, 2))
+    time.sleep(5)
+
